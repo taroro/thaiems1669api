2441b90dfacad002f7a0f368618ee6f071ae4933
Artid Boonrerng <artid.boonrerng@gmail.com>
2017-05-31 14:27:54 +0700

    31/5/2017

--- config/app.php
+++ config/app.php
@@ -180,7 +180,7 @@ return [
         /*
          * Mapper Service Providers...
          */
-        Cornford\Googlmapper\MapperServiceProvider::class,
+        //Cornford\Googlmapper\MapperServiceProvider::class,
     ],
 
     /*
@@ -229,7 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Mapper' => Cornford\Googlmapper\Facades\MapperFacade::class,
+        //'Mapper' => Cornford\Googlmapper\Facades\MapperFacade::class,
 
     ],
 
